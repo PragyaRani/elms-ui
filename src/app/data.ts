e7215943c2151c08f8c0a436d516f916f907c992
cogdotnet1659 <cogdotnet1659@CTSDOTNET147>
2022-11-15 11:24:35 +0530

    sample

---
+++ src/app/data.ts
@@ -0,0 +1,33 @@
+CourseId	Name	Tag	Rating	Author	Language	Hours	CategoriesId	ContentsId	TopicsId	StudentsUserId
+1	Javascript for Professional		0	Max	Hindi	8.00	3	2	7	NULL
+2	C# for Beginners		0	Max	English	5.00	3	2	6	NULL
+3	Javascript for beginners		0	Max	Hindi	3.00	3	2	7	NULL
+
+
+UserId	Name	Address	City	Pin	State	Phone	Email	Password	Role
+1	Pragya	Manyata	Bangalore	560030	Karnataka	78997654	pragya@gmail.com	pragya@123	Student
+2	pragya	manyata	bangalore	456987	karnataka	456321789	pragya.instuctor@gmail.com	pragya@123	Instuctor
+3	pragya	manyata	bangalore	456987	karnataka	456321789	pragya.admin@gmail.com	pragya@123	Admin
+4	Ashton	st murray	Oxforf	456741	California	741852963	ashton@gmail.com	ash@123	NULL
+5	John	manyata	bangalore	456987	karnataka	456321789	john@gmail.com	john@123	NULL
+
+Id	CategoryName	SubCategory	TopicsId
+1	Cloud Coputing	Cloud Certification	2
+2	Development	Mobile Development	8
+3	Development	Game Development	5
+4	Design	Web Design	4
+
+Id	ContentName	Resource
+1	Video	NULL
+2	Book	NULL
+3	External Url	NULL
+
+Id	TopicName
+1	Kuburnets
+2	AWS Certification
+3	CSS
+4	HTML
+5	C#
+6	Javascript
+7	Angular
+8	Java
